927e515bfc95980f1d40bcb6e89306c84a4b3101
fizzoo <vixirfizz@gmail.com>
2016-01-01 18:26:24 +0100

    less accidental quitting, indentation strategy change

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,8 @@
 import XMonad
-import XMonad.Layout.Grid
 import qualified Data.Map as M -- for keys
+import System.Exit
 
+import XMonad.Layout.Grid
 import XMonad.Hooks.ManageHelpers -- doFullFloat, isFullScreen etc
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
@@ -23,15 +24,17 @@ minLayoutHook = Tall 1 (1/40) (1/2) ||| Full ||| Grid
 
 minaKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
     [ ((modMask, xK_Return), spawn $ XMonad.terminal conf)
-    , ((modMask, xK_d     ), spawn "dmenu_run")
-    , ((modMask, xK_s     ), spawn "termite -t statusy -e htop")
-    , ((modMask, xK_c     ), spawn "chromium")
-    , ((modMask, xK_y     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in PATH: \"$PATH\"; fi")
-    , ((modMask, xK_q     ), kill)
-    , ((modMask, xK_F1    ), spawn "xbacklight = 4")
-    , ((modMask, xK_F2    ), spawn "xbacklight = 16")
-    , ((modMask, xK_F3    ), spawn "xbacklight = 50")
-    , ((modMask, xK_F4    ), spawn "xbacklight = 100")
+    , ((modMask, xK_d), spawn "dmenu_run")
+    , ((modMask, xK_s), spawn "termite -t statusy -e htop")
+    , ((modMask, xK_c), spawn "chromium")
+    , ((modMask, xK_y), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in PATH: \"$PATH\"; fi")
+    , ((modMask .|. shiftMask, xK_y), io (exitWith ExitSuccess)) -- Quit xmonad
+    , ((modMask, xK_q), kill)
+    , ((modMask .|. shiftMask, xK_q), kill) -- accidently quit otherwise
+    , ((modMask, xK_F1), spawn "xbacklight = 4")
+    , ((modMask, xK_F2), spawn "xbacklight = 16")
+    , ((modMask, xK_F3), spawn "xbacklight = 50")
+    , ((modMask, xK_F4), spawn "xbacklight = 100")
     ]
 
 minManageHook = composeAll
