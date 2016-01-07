264521d1ad3bf0f607db6bab9dac0b3cfd838996
fizzoo <vixirfizz@gmail.com>
2016-01-07 23:00:20 +0100

    fine, avoid struts. various tweaks

--- xmonad.hs
+++ xmonad.hs
@@ -2,25 +2,28 @@ import XMonad
 import qualified Data.Map as M -- for keys
 import System.Exit
 
-import XMonad.Layout.Grid
 import XMonad.Hooks.ManageHelpers -- doFullFloat, isFullScreen etc
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog -- for xmobar
 import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.EwmhDesktops -- fullscreenEventHook, emwh etc
+import XMonad.Hooks.ManageDocks -- avoidStruts, manageDocks, docksEventHook etc
+import XMonad.Layout.GridVariants --grid variant that tiles the first two vertically
 
 main = xmonad $ ewmh defaultConfig
     { modMask = mod4Mask
     , terminal = "termite"
     , borderWidth = 0
     , layoutHook = minLayoutHook
-    , manageHook = minManageHook
-    , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
+    , manageHook = minManageHook <+> manageDocks
+    , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook <+> docksEventHook
     , keys = minaKeys <+> keys defaultConfig
     , logHook = fadeInactiveCurrentWSLogHook 0.90 <+> -- fade inactive windows on active workspace
         dynamicLogWithPP xmobarPP {ppOrder = (\(ws:_:_:_) -> [ws]) } -- log in xmobar format, take only the workspace part (and notifications, where applicable)
     }
 
-minLayoutHook = Tall 1 (1/40) (1/2) ||| Full ||| Grid
+minLayoutHook = avoidStruts grid ||| Full
+    where
+    grid = Grid (16/9) -- first 3 windows are exactly like tall, then degrades gracefully into grids
 
 minaKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
     [ ((modMask, xK_Return), spawn $ XMonad.terminal conf)
@@ -29,7 +32,7 @@ minaKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
     , ((modMask, xK_c), spawn "chromium")
     , ((modMask, xK_p), spawn "pavucontrol")
     , ((modMask, xK_y), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in PATH: \"$PATH\"; fi")
-    , ((modMask .|. shiftMask, xK_y), io (exitWith ExitSuccess)) -- Quit xmonad
+    , ((modMask .|. shiftMask, xK_y), io (exitSuccess)) -- Quit xmonad
     , ((modMask, xK_q), kill)
     , ((modMask .|. shiftMask, xK_q), kill) -- accidently quit otherwise
     , ((modMask, xK_F1), spawn "xbacklight = 4")
