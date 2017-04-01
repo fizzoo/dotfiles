0885598582013ff6f8b0086c006c500431638e35
fizzoo <vixirfizz@gmail.com>
2017-04-01 17:36:26 +0200

    Clearer xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -42,38 +42,45 @@ myterm = "st"
 -- of all active bindings.
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf = M.fromList $
-  [ ((mod4Mask, xK_Return), spawn $ XMonad.terminal conf)
-  , ((mod4Mask, xK_Tab), windows W.focusDown)
-  , ((mod4Mask .|. shiftMask, xK_Tab), windows W.focusUp)
-  , ((mod4Mask, xK_j), windows W.focusDown)
-  , ((mod4Mask, xK_k), windows W.focusUp)
-  , ((mod4Mask .|. shiftMask, xK_j), windows W.swapDown)
-  , ((mod4Mask .|. shiftMask, xK_k), windows W.swapUp)
-  , ((mod4Mask, xK_d), spawn "dmenu_run -b -i -nf \"#888888\" -nb \"#2D1F21\" -sf \"#ffffff\" -sb \"#6D1F21\" -fn \"Dina-10\" -l 12")
-  , ((mod4Mask, xK_s), spawn $ myterm ++ " -t fully -e htop")
-  , ((mod4Mask, xK_x), spawn "emacs")
-  , ((mod4Mask, xK_c), spawn "firefox")
-  , ((mod4Mask, xK_o), spawn "swapsinks")
-  , ((mod4Mask, xK_p), spawn "pavucontrol")
-  , ((0, xK_Print), spawn "maim -s ~/maim$(date +%s).png")
-  , ((0, 0x1008ff45), spawn "maim -s ~/maim$(date +%s).png") -- F14
-  , ((mod4Mask, xK_Delete), spawn "susp")
-  , ((0, 0x1008ff49), spawn "sleep 0.1 && xset dpms force off") -- F18
-  , ((mod4Mask, xK_y), spawn "xmonad --recompile && xmonad --restart")
-  , ((mod4Mask .|. shiftMask, xK_y), io exitSuccess)
-  , ((mod4Mask, xK_q), kill)
-  , ((mod4Mask, xK_F1), spawn "sudo backlight 64")
-  , ((mod4Mask, xK_F2), spawn "sudo backlight 200")
-  , ((mod4Mask, xK_F3), spawn "sudo backlight 400")
-  , ((mod4Mask, xK_F4), spawn "sudo backlight 852")
-  , ((mod4Mask, xK_t), sinkAll >> setLayout (XMonad.layoutHook conf))
-  , ((mod4Mask, xK_a), sinkAll >> floatAllCurrent)
-  , ((mod4Mask, xK_f), sendMessage NextLayout)
+  [ ((mod4Mask, xK_Return),            spawn $ XMonad.terminal conf)
+  , ((mod4Mask, xK_Tab),               down)
+  , ((mod4Mask .|. shiftMask, xK_Tab), up)
+  , ((mod4Mask, xK_j),                 down)
+  , ((mod4Mask, xK_k),                 up)
+  , ((mod4Mask .|. shiftMask, xK_j),   swap down)
+  , ((mod4Mask .|. shiftMask, xK_k),   swapup)
+  , ((mod4Mask, xK_d),                 spawn "dmenu_run -b -i -nf \"#888888\" -nb \"#2D1F21\" -sf \"#ffffff\" -sb \"#6D1F21\" -fn \"Dina-10\" -l 12")
+  , ((mod4Mask, xK_s),                 spawn $ myterm ++ " -t fully -e htop")
+  , ((mod4Mask, xK_x),                 spawn "emacs")
+  , ((mod4Mask, xK_c),                 spawn "firefox")
+  , ((mod4Mask, xK_o),                 spawn "swapsinks")
+  , ((mod4Mask, xK_p),                 spawn "pavucontrol")
+  , ((0, xK_Print),                    maim)
+  , ((0, 0x1008ff45),                  maim) -- F14
+  , ((mod4Mask, xK_Delete),            spawn "susp")
+  , ((0, 0x1008ff49),                  spawn "sleep 0.1 && xset dpms force off") -- F18
+  , ((mod4Mask, xK_y),                 spawn "xmonad --recompile && xmonad --restart")
+  , ((mod4Mask .|. shiftMask, xK_y),   io exitSuccess)
+  , ((mod4Mask, xK_q),                 kill)
+  , ((mod4Mask, xK_F1),                spawn "sudo backlight 64")
+  , ((mod4Mask, xK_F2),                spawn "sudo backlight 200")
+  , ((mod4Mask, xK_F3),                spawn "sudo backlight 400")
+  , ((mod4Mask, xK_F4),                spawn "sudo backlight 852")
+  , ((mod4Mask, xK_t),                 sinkAll >> setLayout (XMonad.layoutHook conf))
+  , ((mod4Mask, xK_a),                 sinkAll >> floatAllCurrent)
+  , ((mod4Mask, xK_f),                 sendMessage NextLayout)
   ]
   ++
   [((m .|. mod4Mask, k), windows $ f i)
     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+  where
+    setmouse = warpToWindow 0.5 0.5
+    down = windows W.focusDown >> setmouse
+    up = windows W.focusUp >> setmouse
+    swapdown = windows W.swapDown >> setmouse
+    swapup = windows W.swapUp >> setmouse
+    maim = spawn "maim -s ~/maim$(date +%s).png"
 
 myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings _ = M.fromList
