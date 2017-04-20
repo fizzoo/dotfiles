858984b78a4b79a05f7584ab1dd5392a67bf7111
fizzoo <vixirfizz@gmail.com>
2017-04-20 19:22:18 +0200

    Added safety xterm bind + shortened bindings xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -43,33 +43,34 @@ myterm = "st"
 -- of all active bindings.
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf = M.fromList $
-  [ ((mod4Mask, xK_Return),            spawn $ XMonad.terminal conf)
-  , ((mod4Mask, xK_Tab),               down)
-  , ((mod4Mask .|. shiftMask, xK_Tab), up)
-  , ((mod4Mask, xK_j),                 down)
-  , ((mod4Mask, xK_k),                 up)
-  , ((mod4Mask .|. shiftMask, xK_j),   swapdown)
-  , ((mod4Mask .|. shiftMask, xK_k),   swapup)
-  , ((mod4Mask, xK_d),                 spawn "dmenu_run -b -i -nf \"#888888\" -nb \"#2D1F21\" -sf \"#ffffff\" -sb \"#6D1F21\" -fn \"Dina-10\" -l 12")
-  , ((mod4Mask, xK_s),                 spawn $ myterm ++ " -t fully -e htop")
-  , ((mod4Mask, xK_x),                 spawn "emacs")
-  , ((mod4Mask, xK_c),                 spawn "firefox")
-  , ((mod4Mask, xK_o),                 spawn "swapsinks")
-  , ((mod4Mask, xK_p),                 spawn "pavucontrol")
-  , ((0, xK_Print),                    maim)
-  , ((0, 0x1008ff45),                  maim) -- F14
-  , ((mod4Mask, xK_Delete),            spawn "susp")
-  , ((0, 0x1008ff49),                  spawn "sleep 0.1 && xset dpms force off") -- F18
-  , ((mod4Mask, xK_y),                 spawn "xmonad --recompile && xmonad --restart")
-  , ((mod4Mask .|. shiftMask, xK_y),   io exitSuccess)
-  , ((mod4Mask, xK_q),                 kill)
-  , ((mod4Mask, xK_F1),                spawn "sudo backlight 64")
-  , ((mod4Mask, xK_F2),                spawn "sudo backlight 200")
-  , ((mod4Mask, xK_F3),                spawn "sudo backlight 400")
-  , ((mod4Mask, xK_F4),                spawn "sudo backlight 852")
-  , ((mod4Mask, xK_t),                 sinkAll >> setLayout (XMonad.layoutHook conf))
-  , ((mod4Mask, xK_a),                 sinkAll >> floatAllCurrent)
-  , ((mod4Mask, xK_f),                 sendMessage NextLayout)
+  [ ((m,  xK_Return),  spawn $ XMonad.terminal conf)
+  , ((ms, xK_Return),  spawn "xterm")
+  , ((m,  xK_Tab),     down)
+  , ((ms, xK_Tab),     up)
+  , ((m,  xK_j),       down)
+  , ((m,  xK_k),       up)
+  , ((ms, xK_j),       swapdown)
+  , ((ms, xK_k),       swapup)
+  , ((m,  xK_d),       spawn "dmenu_run -b -i -nf \"#888888\" -nb \"#2D1F21\" -sf \"#ffffff\" -sb \"#6D1F21\" -fn \"Dina-10\" -l 12")
+  , ((m,  xK_s),       spawn $ myterm ++ " -t fully -e htop")
+  , ((m,  xK_x),       spawn "emacs")
+  , ((m,  xK_c),       spawn "firefox")
+  , ((m,  xK_o),       spawn "swapsinks")
+  , ((m,  xK_p),       spawn "pavucontrol")
+  , ((0,  xK_Print),   maim)
+  , ((0,  0x1008ff45), maim) -- F14
+  , ((m,  xK_Delete),  spawn "susp")
+  , ((0,  0x1008ff49), spawn "sleep 0.1 && xset dpms force off") -- F18
+  , ((m,  xK_y),       spawn "xmonad --recompile && xmonad --restart")
+  , ((ms, xK_y),       io exitSuccess)
+  , ((m,  xK_q),       kill)
+  , ((m,  xK_F1),      spawn "sudo backlight 64")
+  , ((m,  xK_F2),      spawn "sudo backlight 200")
+  , ((m,  xK_F3),      spawn "sudo backlight 400")
+  , ((m,  xK_F4),      spawn "sudo backlight 852")
+  , ((m,  xK_t),       sinkAll >> setLayout (XMonad.layoutHook conf))
+  , ((m,  xK_a),       sinkAll >> floatAllCurrent)
+  , ((m,  xK_f),       sendMessage NextLayout)
   ]
   ++
   [((m .|. mod4Mask, k), windows $ f i)
@@ -82,6 +83,8 @@ myKeys conf = M.fromList $
     swapdown = windows W.swapDown >> setmouse
     swapup = windows W.swapUp >> setmouse
     maim = spawn "maim -s ~/maim$(date +%s).png"
+    m = mod4Mask
+    ms = mod4Mask .|. shiftMask
 
 myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings _ = M.fromList
